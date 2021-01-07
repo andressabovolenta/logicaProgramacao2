f3c93b99f97605361bd41e3914beecb4618eb273
Andressa Bovolenta <andressa.bbovolenta@gmail.com>
2021-01-07 17:50:59 -0300

    first commit

---
+++ LogicaProgramacao2/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+
+namespace LogicaProgramacao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ESTRUTURA DE REPETIÇÃO:
+
+            //Console.WriteLine("Qual o nome do atual presidente do Brasil?");
+            //Console.WriteLine("1 - Fernando Henrique Cardoso");
+            //Console.WriteLine("2 - Fernando Collor de Mello");
+            //Console.WriteLine("3 - Jair Adolfo Messias Hitler Bolsonaro");
+            //Console.WriteLine("4 - Luiz Inácio Lula da Silva");
+            //Console.WriteLine("5 - Dilma Roussef");
+            //Console.WriteLine("6 - Tancredo Neves");
+            //Console.WriteLine("7 - Jânio Quadros");
+            //Console.WriteLine("8 - Getúlio Vargas");
+            //Console.WriteLine("9 - Juscelino Kubitschek");
+            //Console.WriteLine("10 - João Goulart");
+
+            //int resposta = 0;
+            //int erro = 0;
+            //int count = 1;
+
+            //while (resposta != 3)
+            //{
+            //    if (count >= 2)
+            //        erro++;
+
+            //    Console.WriteLine("1 - Fernando Henrique Cardoso");
+            //    Console.WriteLine("2 - Fernando Collor de Mello");
+            //    Console.WriteLine("3 - Jair Adolfo Messias Hitler Bolsonaro");
+            //    Console.WriteLine("4 - Luiz Inácio Lula da Silva");
+            //    Console.WriteLine("5 - Dilma Roussef");
+            //    Console.WriteLine("6 - Tancredo Neves");
+            //    Console.WriteLine("7 - Jânio Quadros");
+            //    Console.WriteLine("8 - Getúlio Vargas");
+            //    Console.WriteLine("9 - Juscelino Kubitschek");
+            //    Console.WriteLine("10 - João Goulart");
+
+            //    resposta = Convert.ToInt32(Console.ReadLine());
+
+            //    count++;
+            //}
+
+            //double nota = 10;
+
+            //if (erro > 0)
+            //    nota = nota / erro;
+
+            //Console.WriteLine($"Certa a resposta! Sua nota é: {nota}");
+
+            //Console.WriteLine("Digite o número inicial:");
+            //int inicial = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Digite o número final:");
+            //int final = Convert.ToInt32(Console.ReadLine());
+
+            //if (final <- inicial)
+            //{
+            //    Console.WriteLine("Erro: o número FINAL não pode ser menor do que o INICIAL.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Números ímpares (I) ou números pares (P):");
+            //    string tipo = Console.ReadLine();
+
+            //    switch (tipo.ToLower())
+            //    {
+            //        case "p":
+
+            //            for (int i = inicial; i <= final; i++)
+            //                if (i % 2 == 0)
+            //                    Console.WriteLine(i);
+            //            break;
+
+            //        case "i":
+
+            //            for (int i = inicial; i < final; i++)
+            //                if (i % 2 == 1)
+            //                    Console.WriteLine(i);
+            //            break;
+
+            //        default:
+            //            Console.WriteLine("Você não digitou o número correto.");
+            //            break;
+            //    }
+            //}
+
+            //While
+
+            //int inteiro = 0;
+            //while (inteiro <= 10)
+            //{
+            //    Console.WriteLine(inteiro);
+            //    inteiro = inteiro + 1;
+            //}
+
+            //int numero = 1;
+            //while (numero % 2 == 1) //ímpar
+            //{
+            //    Console.WriteLine("Digite um número par:");
+            //    numero = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.WriteLine("Obrigada por digitar um número par!");
+
+            //For
+
+            //for (int i = 1; i <= 50; i++)
+            //{
+            //    if (i % 2 == 0) //par
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+            //ESTRUTURA DE DADOS
+
+            //pilha - Last In, First Out (LIFO)
+            //int[] numeros = new int[10];
+
+            //ESTRUTURA DE ATRIBUIÇÃO DE DADOS:
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    numeros[i] = i;
+            //}
+            //for (int i = 9; i >= 0; i--)
+            //{
+            //    Console.WriteLine(numeros[i]);
+            //}
+                        
+
+            //fila - First In, First Out (FIFO)
+
+            //int[] numeros = new int[10];
+
+            ////ESTRUTURA DE ATRIBUIÇÃO DE DADOS:
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    numeros[i] = i;
+            //}
+
+            ////ESTRUTURA DE IMPRESSÃO DE DADOS
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(numeros[i]);
+            //}
+
+
+            //lista - lista não organizada (random)
+
+            //int[] numeros = new int[5];
+            //numeros[3] = 1;
+            //numeros[1] = 100;
+            //Console.WriteLine(numeros[3]);
+            //Console.WriteLine(numeros[1]);
+
+
+            // árvore e matriz
+        }
+    }
+}
